bc18f835e7253776ad76c1997e0ffc79a1c86520
Cesar Augusto Diaz Arriaga <cesar.a.diaz@gmail.com>
2019-04-12 21:41:02 -0500

    Initial commit

---
+++ buildAndRun.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git pull https://github.com/cesardiaz-dev/microservicios-java.git
+mvn clean package && docker build -t co.edu.utp.microservicios/hola-mundo .
+docker rm -f hola-mundo || true && docker run -d -p 8080:8080 -p 4848:4848 --name hola-mundo co.edu.utp.microservicios/hola-mundo 
